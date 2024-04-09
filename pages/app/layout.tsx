104c38ca41a0fa477b2187c1574e981efb357b4b
zuk246 <tadasukeiizuka@gmail.com>
2024-04-09 18:09:14 +0900

    update: layout

--- pages/app/layout.tsx
+++ pages/app/layout.tsx
@@ -2,10 +2,24 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Providers } from './providers';
 import Header from '@/components/base/Header';
+import Footer from '@/components/base/Footer';
 
 export const metadata: Metadata = {
     title: 'zuk246',
-    description: 'zuk246のポートフォリサイト',
+    description: `zuk246's portfolio site`,
+    openGraph: {
+        locale: 'en_US',
+        type: 'website',
+        url: 'https://zuk246.net',
+        title: 'zuk246',
+        description: `zuk246's portfolio site`,
+        siteName: 'zuk246',
+    },
+    twitter: {
+        creator: '@zuk246',
+        site: '@zuk246',
+        card: 'summary',
+    },
 };
 
 export default function RootLayout({
@@ -15,10 +29,13 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='ja'>
-            <body>
+            <body className='font-noto'>
                 <Providers>
                     <Header />
-                    <main className='max-w-3xl mx-auto'>{children}</main>
+                    <main className='max-w-2xl mx-auto py-12 text-default-800'>
+                        {children}
+                    </main>
+                    <Footer />
                 </Providers>
             </body>
         </html>
